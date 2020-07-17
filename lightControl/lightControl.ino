76335ad58584c73b8cd1b58f6aa26365907517d9
unknown <cdwatts2001@gmail.com>
2020-07-17 18:31:15 -0400

    fixed version #

--- lightControl/lightControl.ino
+++ lightControl/lightControl.ino
@@ -8,7 +8,7 @@
 
 //Meta - TODO: UPDATE FOR EVERY NEW UNIT - ~~~~~~~~~~~~~ NOTICE ~
   String lightNum = "";
-  const String VERSION = "0.4.7.2";
+  const String VERSION = "0.4.7.3";
   const int ledDataPin = 5;
   const int statusLed = 2;
 
